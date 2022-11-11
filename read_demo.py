ee9aa18e96f08eb2acd0476d5d290899683e990a
N-Sirun <88889025+N-Sirun@users.noreply.github.com>
2022-11-11 22:24:50 +0400

    f

--- read_demo.py
+++ read_demo.py
@@ -1,6 +1,6 @@
-# f = open("testdemo.txt", "r")
-# print(f.readline())
-# f.close()
+f = open("testdemo.txt", "r")
+print(f.readline())
+f.close()
 
 f = open("testdemo.txt", "r+")
 f.write("Some text")
--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
-x = 600
-y  = 900
+x = 6
+y  = 9
 print(id(x))
 print(id(y))
 
